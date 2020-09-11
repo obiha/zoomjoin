e14560a6f3a0ba58284eb108241856061e3d88d2
Joshua Obiha <joshua.obiha@hotmail.com>
2020-09-10 22:32:44 -0400

    Update zoomjoin.py

--- zoomjoin.py
+++ zoomjoin.py
@@ -1,17 +1,38 @@
-import schedule, time
+import schedule, time, webbrowser
 
-def job():
-    print("Something's up")
+def open2406():
+    print("Lecture is about to begin...")
+    webbrowser.open('http://google.com')
 
+def open3005():
+    print("Lecture is about to begin...")
+    webbrowser.open('http://google.com')
 
+def open3008():
+    print("Lecture is about to begin...")
+    webbrowser.open('http://google.com')
+
+def open3203():
+    print("Lecture is about to begin...")
+    webbrowser.open('http://google.com')
+
+def open4001():
+    print("Lecture is about to begin...")
+    webbrowser.open('http://google.com')    
 
 #Time
-# schedule.day(1).seconds.do(job)
 
 schedule.every().thursday.at("22:10").do(job)
+schedule.every().thursday.at("22:10").do(job)
+schedule.every().thursday.at("22:10").do(job)
+schedule.every().thursday.at("22:10").do(job)
+schedule.every().thursday.at("22:10").do(job)
+
+
 while True:
     schedule.run_pending()
-    print("Working")
+    print("online-class CONNECT")
     time.sleep(1)
+
 
 
